2bb4ced0dc0c2dd9cb7a91822a1af360bb6eb92b
alfonso <alfonso96xd@gmail.com>
2021-08-13 13:14:13 -0500

    Add golang application

---
+++ mini-project/backend/src/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+    "net/http"
+	"os"
+	"time"
+)
+
+type HandsOn struct {
+	Time	   time.Time  `json:"time"`
+	HostName   string     `json:"hostname"`
+}
+
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	resp := HandsOn{
+		Time: time.Now(),
+		HostName: os.Getenv("HOSTNAME"),
+	}
+
+	jsonResp, err := json.Marshal(&resp)
+	
+	if err != nil {
+		w.Write([]byte("Error"))
+		return
+	}
+
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+    w.Write(jsonResp)
+}
+
+func main() {
+    http.HandleFunc("/", ServeHTTP)
+    http.ListenAndServe(":8080", nil)
+}
